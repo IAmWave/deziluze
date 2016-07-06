5e9edc7220e38f56a714339449af35f55e8eb118
Václav <vaclav.volhejn@gmail.com>
2016-07-07 00:16:12 +0200

    Added performers. Visual changes.
    New sans-serif primary font; a bunch of smaller style changes

--- dist/js/main.js
+++ dist/js/main.js
@@ -25,6 +25,7 @@ $('.nav a').on('click', function () {
 });
 
 
+//--------------------------VIDEO-----------------------------
 var vid = $("#intro-video");
 var wrapper = $('.wrapper-outer');
 
@@ -62,3 +63,62 @@ $(document).ready(function(){
         vid[0].pause();
     }
 });
+
+
+//--------------------------UCINKUJICI---------------------------
+
+$('#modal-inner').click(function (event) {
+        event.stopPropagation();
+    });
+
+var dataJSON;
+
+$.getJSON("/js/data.json", function (data) {
+    dataJSON = data;
+});
+
+$(".event").click(function(event){
+    if(!dataJSON) return;
+    
+    if(!makeDescription(event.target)) return;
+
+    $('#modal').css({
+        "visibility": "visible",
+        "opacity": 1,
+        "overflow-y": "scroll"
+    });
+
+    $('body').width($('body').width());
+    $('body').addClass("noscroll");
+    
+    $(document).keydown(function (e) { //escape = click
+        if (e.keyCode === 27) $('#modal').click();
+    });
+    $('#modal').click(function () {
+        $('#modal').css({
+            "visibility": "hidden",
+            "opacity": 0,
+            "overflow-y": "hidden"
+        });
+        $('body').removeClass("noscroll").width("100%");
+    });
+});
+
+var makeDescription = function(element){
+    if(!dataJSON.ucinkujici[element.id]) return false;
+    var data = dataJSON.ucinkujici[element.id];
+    var target = $('#modal-inner');
+    target.empty();
+    target.append("<h1>"+element.innerHTML+"</h1>");
+    if(data.desc){
+        data.desc = data.desc.replace("\n", "</p><p>");
+        target.append("<p>"+data.desc+"</p>");
+    }
+    if(data.sample) {
+        target.append("<p><a target='_blank' href=\""+data.sample+"\">Ukázka</a></p>");
+    }
+    if(data.site) {
+        target.append("<p><a target='_blank' href=\""+data.site+"\">Stránky</a></p>");   
+    }
+    return true;
+};
